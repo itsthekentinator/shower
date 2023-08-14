f27dffbce0b208aa149f659de7c7e7040f5cfa99
itsthekentinator <134373636+itsthekentinator@users.noreply.github.com>
2023-08-14 09:45:02 +0900

    Update sript.js

--- sript.js
+++ sript.js
@@ -1,4 +1,4 @@
 // script.js
 function start() {
-  window.location.href = "https://music.apple.com/jp/playlist/shower-bath/pl.u-6mo4MleCBqy2qvq?l=en-US"; // Replace with actual link
+  window.location.href = "https://music.apple.com/jp/playlist/shower-bath/pl.u-6mo4MleCBqy2qvq?l=en-US";
 }
